72dc9da7be8d4947cc0619583170ced5378203ba
Leonte88 <catalin.leonte88@gmail.com>
2016-09-28 01:45:32 +0300

    one FOUND item added for switch lists test

--- mysql/lostfound.sql
+++ mysql/lostfound.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.7.1
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 21, 2016 at 08:39 PM
--- Server version: 5.5.39
--- PHP Version: 5.4.31
+-- Generation Time: Sep 28, 2016 at 12:44 AM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 5.6.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,13 +14,11 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `lostfound`
 --
-CREATE DATABASE IF NOT EXISTS `lostfound` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `lostfound`;
 
 -- --------------------------------------------------------
 
@@ -28,9 +26,8 @@ USE `lostfound`;
 -- Table structure for table `items`
 --
 
-DROP TABLE IF EXISTS `items`;
-CREATE TABLE IF NOT EXISTS `items` (
-`id` int(11) NOT NULL,
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL,
   `name` varchar(300) NOT NULL,
   `description` text NOT NULL,
   `lat` double NOT NULL,
@@ -39,7 +36,7 @@ CREATE TABLE IF NOT EXISTS `items` (
   `whenDate` datetime NOT NULL,
   `itemType` enum('LOST','FOUND') NOT NULL,
   `userid` int(11) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=41 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `items`
@@ -47,7 +44,8 @@ CREATE TABLE IF NOT EXISTS `items` (
 
 INSERT INTO `items` (`id`, `name`, `description`, `lat`, `lng`, `address`, `whenDate`, `itemType`, `userid`) VALUES
 (39, 'dog', 'dedd\n', 46.1492125, 24.7665742, 'DJ106, Şaeş 547042, Romania', '2016-09-21 19:35:19', 'LOST', 6),
-(40, 'apa', 'dada', 45.7316886, 21.2474649, 'Bulevardul Sudului, Timișoara, Romania', '2016-09-21 20:07:25', 'LOST', 6);
+(40, 'apa', 'dada', 45.7316886, 21.2474649, 'Bulevardul Sudului, Timișoara, Romania', '2016-09-21 20:07:25', 'LOST', 6),
+(41, 'Telefon', 'Samsung, alb', 46.1492125, 24.7665742, 'DJ106, Şaeş 547042, Romania', '2016-09-21 19:35:19', 'FOUND', 14);
 
 -- --------------------------------------------------------
 
@@ -55,13 +53,12 @@ INSERT INTO `items` (`id`, `name`, `description`, `lat`, `lng`, `address`, `when
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-`id` int(11) NOT NULL,
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` mediumtext NOT NULL,
   `telephone` varchar(14) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
@@ -71,7 +68,8 @@ INSERT INTO `users` (`id`, `username`, `password`, `telephone`) VALUES
 (6, 'adam', '14bd76e0219841c78ab65227ea794', '1234'),
 (7, 'adam1', '14bd76e0219841c78ab65227ea794', '1234'),
 (8, 'adam12', '14bd76e0219841c78ab65227ea794', '12'),
-(13, 'adam22', '14bd76e0219841c78ab65227ea794', '#');
+(13, 'adam22', '14bd76e0219841c78ab65227ea794', '#'),
+(14, 'leonte', 'd8578edf8458ce6fbc5bb76a58c5ca4', '0746883826');
 
 --
 -- Indexes for dumped tables
@@ -81,13 +79,14 @@ INSERT INTO `users` (`id`, `username`, `password`, `telephone`) VALUES
 -- Indexes for table `items`
 --
 ALTER TABLE `items`
- ADD PRIMARY KEY (`id`), ADD KEY `userid` (`userid`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userid` (`userid`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -97,12 +96,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=41;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- Constraints for dumped tables
 --
@@ -111,7 +110,7 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
 -- Constraints for table `items`
 --
 ALTER TABLE `items`
-ADD CONSTRAINT `items_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `items_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
